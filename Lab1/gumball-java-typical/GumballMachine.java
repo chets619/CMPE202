c3fd5954c6cfb5c07f6e5b941ed6a2bb296bd13c
Chaitanya Naik <chaitanyanaik619@gmail.com>
2019-09-10 19:20:24 -0700

    Multiple gumball machine problem

--- Lab1/gumball-java-typical/GumballMachine.java
+++ Lab1/gumball-java-typical/GumballMachine.java
@@ -1,43 +1,91 @@
+import java.util.Scanner;
 
 public class GumballMachine
 {
 
     private int num_gumballs;
     private boolean has_quarter;
+    private int gumball_price;
+    private int inserted_value;
+    private int machineType;
 
-    public GumballMachine( int size )
+    public GumballMachine( int size, int type )
     {
         // initialise instance variables
         this.num_gumballs = size;
-        this.has_quarter = false;
+        this.machineType = type;
+        this.gumball_price = type == 1 ? 25 : 50;
+        selectAction();
+    }
+    
+    private int recordTextResponse(String str) {
+        StringBuffer string = new StringBuffer();
+        string.append(str);
+        System.out.println(string);            
+        
+        Scanner sc = new Scanner(System.in);            
+        int option = sc.nextInt();
+        
+        return option;
+    }
+    
+    private void selectAction() {
+        int option = recordTextResponse("Select Action:\n1.Enter Coin\n2.Turn Crank");       
+        
+        switch(option) {
+            case 1:
+                acceptCoin();
+                break;
+            case 2:
+                turnCrank();
+                break;
+            default:
+                System.out.println("Invalid Option! Select again...");
+                selectAction();
+        }
+    }
+    
+    private void acceptCoin() {
+        int[] coinArray = new int[]{ 25, 5, 10 };          
+        int option = recordTextResponse("Select which coin to enter:\n1.Quarter\n2.Nickel(5 cents)\n3.Dime(10 cents)");
+        
+        insertCoin(coinArray[option - 1]);
     }
 
-    public void insertQuarter(int coin)
+    public void insertCoin(int coin)
     {
-        if ( coin == 25 )
-            this.has_quarter = true ;
-        else 
-            this.has_quarter = false ;
+        System.out.println(coin);
+        
+        if (this.machineType != 3 && coin != 25)
+            System.out.println("Invalid Coin...Coin returned");
+        else {
+            this.inserted_value += coin;
+            System.out.println("Total Value: " + this.inserted_value);
+            selectAction();
+        }
     }
     
     public void turnCrank()
     {
-    	if ( this.has_quarter )
-    	{
-    		if ( this.num_gumballs > 0 )
-    		{
-    			this.num_gumballs-- ;
-    			this.has_quarter = false ;
-    			System.out.println( "Thanks for your quarter.  Gumball Ejected!" ) ;
-    		}
-    		else
-    		{
-    			System.out.println( "No More Gumballs!  Sorry, can't return your quarter." ) ;
-    		}
-    	}
-    	else 
-    	{
-    		System.out.println( "Please insert a quarter" ) ;
-    	}        
+        if ( this.inserted_value >= this.gumball_price )
+        {
+            if ( this.num_gumballs > 0 )
+            {
+                this.num_gumballs-- ;
+                System.out.println("Coins returned:" + (this.inserted_value - this.gumball_price));
+                this.inserted_value = 0 ;
+                System.out.println( "Thanks for your quarter.  Gumball Ejected!" ) ;
+                selectAction();
+            }
+            else
+            {
+                System.out.println( "No More Gumballs!  Sorry, can't return your quarter." ) ;
+            }
+        }
+        else 
+        {
+            System.out.println( "Please insert appropriate amount" ) ;
+            selectAction();
+        }        
     }
 }
--- Lab1/gumball-java-typical/Main.java
+++ Lab1/gumball-java-typical/Main.java
@@ -1,22 +1,17 @@
-
+import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) {
-		GumballMachine gumballMachine = new GumballMachine(5);
-
-		System.out.println(gumballMachine);
-
-		gumballMachine.insertQuarter( 25 );
-		gumballMachine.turnCrank();
-
-		System.out.println(gumballMachine);
-
-		gumballMachine.insertQuarter( 25 );
-		gumballMachine.turnCrank();
-		gumballMachine.insertQuarter( 25 );
-		gumballMachine.turnCrank();
-
-		System.out.println(gumballMachine);
+    public static void main(String[] args) {
+            GumballMachine gumballMachine;
+            StringBuffer string = new StringBuffer();
+            string.append("Please enter the number of machine to be used:\n1.Quarter Machine\n2.Two Quarter Machine\n3.All Coins Machine");
+            System.out.println(string);            
+            
+            Scanner sc = new Scanner(System.in);            
+            int option = sc.nextInt();    
+            
+            
+            gumballMachine = new GumballMachine(5, option);
 	}
 }
